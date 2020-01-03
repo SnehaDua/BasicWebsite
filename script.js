47292a619ea97aac23e13ba2a2f49b001e0b36c3
Sneha Dua <sneha.dua10@gmail.com>
2020-01-03 23:29:40 +1000

    JS basics

---
+++ script.js
@@ -0,0 +1,24 @@
+Enter one expression that satisfies all of the following criteria:
+
+The answer evaluates to 14.
+The expression contains at least 3 different values.
+The expression contains exactly 1 set of parentheses.
+-----------
+
+2*6+(6/3)
+...............................................................................
+
+Using the modulus operator %, enter an expression with exactly two values that results in a remainder of 4.
+----------
+39%5
+..................................................................................
+Write a boolean expression that checks whether two numbers are equal and returns the value false.
+----------
+
+12==34
+..........................................................................
+Write a boolean expression that checks whether two numbers are not equal and returns the value true.
+----------
+12!=34
+
+................................................................................
