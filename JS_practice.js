96a66ae46091b5c0e17c593feb3e3333d71a01aa
Sneha Dua <sneha.dua10@gmail.com>
2020-01-08 02:05:41 +1000

    71% of basic JS

--- JS_practice.js
+++ JS_practice.js
@@ -71,3 +71,44 @@ function timesFive(n) {
 console.log(minusSeven(10));
 console.log(timesFive(0));
 ----------------------------------------------------------
+function testGreaterOrEqual(val) {
+  if (val>=20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val>=10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+// Change this value to test
+testGreaterOrEqual(10);
+----------------------------------------------------
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+   // Only change code below this line
+if(strokes == 1){
+  return names[0];
+}else if(strokes <= (par-2)) {
+  return names[1];
+}else if(strokes == par-1) {
+  return names[2];
+}else if(strokes == par) {
+  return names[3];
+}else if(strokes == (par+1)) {
+  return names[4];
+}else if(strokes == (par+2)) {
+  return names[5];
+}else{
+  return names[6];
+}
+
+ // return "Change Me";
+  // Only change code above this line
+}
+
+// Change these values to test
+golfScore(5, 4);
+--------------------------------------------------------
