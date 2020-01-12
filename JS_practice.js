8be3cec578aa265ea243a8441a05b62ef8b23598
Sneha Dua <sneha.dua10@gmail.com>
2020-01-12 22:48:31 +1000

    recursion

--- JS_practice.js
+++ JS_practice.js
@@ -309,3 +309,16 @@ do {
 }while (i <  5);
 --------------------------------------------------------------------------------------
 
+function sum(arr, n) {
+  // Only change code below this line
+if (n<=0){
+  return arr[0];
+} else {
+  return sum(arr,n-1)+arr[n];
+}
+}
+  // Only change code above this line
+
+
+---------------------------------------------------------------------------------
+
