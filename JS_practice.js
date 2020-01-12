70bafce09c91ab711e2f081b68718f038264def3
Sneha Dua <sneha.dua10@gmail.com>
2020-01-12 22:31:01 +1000

    95/110 practice

--- JS_practice.js
+++ JS_practice.js
@@ -239,3 +239,47 @@ var myPlants = [
 var secondTree = myPlants[1].list[1]; // Change this line
 
 ---------------------------------------------------------------------------
+// Setup
+var myArray = [];
+var i = 5;
+while(i>=0){
+    myArray.push(i);
+    i--;
+}
+// Only change code below this line.
+---------------------------------------------------------------------------
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+ 
+ for(var i=1;i<=9;i=i+=2){
+myArray.push(i);
+ }
+// Only change code below this line.
+
+-----------------------------------------------------------------------------
+
+// Example
+var ourArr = [ 9, 10, 11, 12];
+var ourTotal = 0;
+
+for (var i = 0; i < ourArr.length; i++) {
+  ourTotal += ourArr[i];
+}
+
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+var total =0;
+for(var i = 0; i<myArr.length; i++){
+  total +=myArr[i];
+}
+// Only change code below this line
+-----------------------------------------------------------------------------------
+
