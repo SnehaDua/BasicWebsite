2202a7988d4fcba257c71bc59346dac3842c055f
Sneha Dua <sneha.dua10@gmail.com>
2020-01-13 14:13:58 +1000

    Math.random() & Math.floor()

--- JS_practice.js
+++ JS_practice.js
@@ -370,4 +370,33 @@ for(var i=0;i<contacts.length;i++){
 lookUpProfile("Akira", "likes");
 ----------------------------------------------------------------------------------------
 
+function randomFraction() {
 
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
+
+--------------------------------------------------------------------------------------
+// Example
+function ourRandomRange(ourMin, ourMax) {
+
+  return Math.floor(Math.random() * (ourMax - ourMin + 1)) + ourMin;
+}
+
+ourRandomRange(1, 9);
+
+// Only change code below this line.
+
+function randomRange(myMin, myMax) {
+
+  return Math.floor(Math.random()*(myMax-myMin+1)+myMin); // Change this line
+
+}
+
+// Change these values to test your function
+var myRandom = randomRange(5, 15);
+
+-----------------------------------------------------------------------------------------------
