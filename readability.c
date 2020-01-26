d164c230fb402b459571ff42c4d955055c06c33f
Sneha Dua <sneha.dua10@gmail.com>
2020-01-26 23:39:39 +1000

    pS2_readability user input

---
+++ readability.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+
+int main(void)
+{
+    string text = get_string("Text : ");
+    int len = strlen(text);
+    printf("length is %d: \n", len);
+    
+    
+   //for(int i = 0; i< )
+}
