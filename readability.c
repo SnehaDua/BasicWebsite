e2dcc8a9655843c869766e3441beab2a6519dd1d
Sneha Dua <sneha.dua10@gmail.com>
2020-02-05 01:34:36 +1000

    errors

--- readability.c
+++ readability.c
@@ -5,10 +5,27 @@
 
 int main(void)
 {
-    string text = get_string("Text : ");
-    int len = strlen(text);
+      string text[] = get_string("Text : ");
+    int len = strlen(text[]);
     printf("length is %d: \n", len);
-    
-    
-   //for(int i = 0; i< )
+
+
+    for(int i = 0; i<len; i++)
+   {
+       if(isalpha(text[i]))
+       {
+           letter_count++;
+
+       }
+       else if(text[i] == " " && isalpha(text[i+1]))
+       {
+           word_count ++;
+       }else if(text[i] == "." || text[i] == "?" || text[i] == "!")
+       {
+           sentence_count ++;
+       }
+   }
+   printf("letter_count : %d \n word_count : %d \n sentence_count : %d \n ",letter_count ,word_count, sentence_count);
+
+
 }
