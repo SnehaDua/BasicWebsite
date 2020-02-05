ad7bd3766c5a2269ab37c524790e1d037a33ddc2
Sneha Dua <sneha.dua10@gmail.com>
2020-02-06 00:11:31 +1000

    basic done bt not working correctly

--- readability.c
+++ readability.c
@@ -2,16 +2,19 @@
 #include <cs50.h>
 #include <string.h>
 #include <ctype.h>
- 
+#include <math.h>
+
+
+void CLF(int, int, int);
 
 int main(void)
 {
     string text = get_string("Text : ");
     int len = strlen(text);
-    printf("length is %d: \n", len);
+   // printf("length is %d: \n", len);
 //printf("and the test u entered is as below:\n %s\n\n",text);
 
-printf("text[i] %c\n", text[0]);
+//printf("text[i] %c\n", text[0]);
 
 
 
@@ -32,10 +35,20 @@ int letter_count = 0, word_count = 0, sentence_count = 0;
            sentence_count ++;
        }
    }
-   printf("letter_count : %d \n word_count : %d \n sentence_count : %d \n ",letter_count ,word_count+1, sentence_count);
-
-
-
+  // printf("letter_count : %d \n word_count : %d \n sentence_count : %d \n ",letter_count ,word_count+1, sentence_count);
+   CLF(letter_count ,word_count+1, sentence_count);
 }
 
+void CLF(Lc,Wc,Sc)
+{
+    float L= 0, S=0;
 
+    L = (Lc/Wc)*100;
+    S = (Sc/Wc)*100;
+
+    float Index = (0.0588 * L) - (0.296 * S) - 15.8;
+    printf(" index : %f \n", Index);
+    int Grade = round(Index);
+    printf(" index is : %d Grade", Grade);
+
+}
